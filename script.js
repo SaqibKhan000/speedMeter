95274be84b419884e13b4104f69ed372b419eba1
SaqibKhan000 <saqibkhannn543@gmail.com>
2025-03-02 23:56:02 +0500

    Speed Testing by Speed Meter

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ let progress = setInterval(() => {
         rgb(252, 35, 6) ${progressStartValue + 40}deg,
         rgb(229, 229, 12) ${progressStartValue * 3}deg,
         rgb(69, 230, 69) ${progressStartValue * 3.3}deg,
-        rgb(255, 255, 255) ${progressStartValue}deg 360deg
+        rgb(239, 236, 236) ${progressStartValue}deg
     )`;
 
-    progressValue.style.rotate = `${progressStartValue * 3.4}deg`;
+    progressValue.style.rotate = `${progressStartValue * 4.5}deg`;
     progressNums.innerHTML = progressStartValue * 2;
 
     if (progressStartValue === progressEndValue) {
