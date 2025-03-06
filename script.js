dfe3adbaef58f66bcf6c337389341ed5ea18ea1d
SaqibKhan000 <saqibkhannn543@gmail.com>
2025-03-06 05:38:50 +0500

    Speed Testing With Speedometer

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let progressNums = document.querySelector(".num-values h2");
 
 let rotationAngle = 0; 
 const maxRotation = 200; 
-const increment = 7; 
+const increment = 8; 
 
 // Function to rotate the needle
 function rotateNeedle() {
     if (rotationAngle <= maxRotation) {
-        progressNums.innerText = rotationAngle - 68;
+        progressNums.innerText = rotationAngle - 75;
         needle.style.transform = `rotate(${rotationAngle}deg)`; 
         rotationAngle += increment; 
         requestAnimationFrame(rotateNeedle); 
